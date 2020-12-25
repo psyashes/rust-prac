0db13cd73398096ee23fc5f2d7c2b86828cc5880
psyashes <43512814+psyashes@users.noreply.github.com>
2020-12-26 00:56:12 +0900

    Add struct and impl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+struct Brothers {
+    big_bro: String,
+    little_bro: String
+}
+
+impl Brothers {
+    fn greet_by_big(&self) -> &Self {
+        println!("I am {}.", self.big_bro);
+        self
+    }
+
+    fn greet_by_little(&self) -> &Self {
+        println!("I am {}.", self.little_bro);
+        self
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let b = Brothers {
+        big_bro: String::from("Mario"),
+        little_bro: String::from("Luigi")
+    };
+
+    b.greet_by_big().greet_by_little();
 }
