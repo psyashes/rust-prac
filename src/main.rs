56eba194aef076a2815884460c7a51dd73d44474
psyashes <43512814+psyashes@users.noreply.github.com>
2020-12-26 13:48:18 +0900

    Add trait

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,35 @@ impl Brothers {
     }
 }
 
+// Trait
+trait Tweet {
+    fn tweet(&self);
+
+    fn tweet_twice(&self) {
+        self.tweet();
+        self.tweet();
+    }
+
+    fn shout(&self) {
+        println!("Uoooooohnnnnnnnn!!!!!");
+    }
+}
+
+struct Dove;
+struct Duck;
+
+impl Tweet for Dove {
+    fn tweet(&self) {
+        println!("Coo!");
+    }
+}
+
+impl Tweet for Duck {
+    fn tweet(&self) {
+        println!("Quack!");
+    }
+}
+
 // main function
 fn main() {
     // construct struct
@@ -32,6 +61,19 @@ fn main() {
         println!("{}", element);
     }
 
+    let dove = Dove {};
+    dove.tweet();
+    dove.tweet_twice();
+    dove.shout();
+
+    let duck = Duck {};
+    
+    let bird_vec: Vec<Box<dyn Tweet>> = vec![Box::new(dove), Box::new(duck)];
+    for bird in bird_vec {
+        bird.tweet();
+    }
+
     // panic
     panic!("In the end.");
 }
+
